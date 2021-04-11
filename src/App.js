ad4143d6c33b29b6707f7daac74077f347c47f98
Bela Hamid <belahamid@gmail.com>
2021-04-11 19:31:01 +0100

    added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
     <>
-    <p>Hello marvellous world</p>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/projects" component={Projects}/>
+        <Route path="/contact" component={Contact}/>
+
+      </Switch>
+    </Router>
 
     </>
     
