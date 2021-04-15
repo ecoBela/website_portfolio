87dbde5de24f14ee62a67e8bcf837ebdf2e9b116
Bela Hamid <belahamid@gmail.com>
2021-04-15 11:11:50 +0100

    jumbotron background image and overlay now renders

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -3,13 +3,32 @@ import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import styled from 'styled-components';
 import globeImage from '../assets/globeImage.jpg';
 
-const Styles = styled.div``;
+const Styles = styled.div`
+.jumbo {
+    background: url(${globeImage}) no-repeat fixed bottom;
+    background-size: cover;
+    colour: pink;
+    height: 200px;
+    position: relative;
+    z-index: -2;
+}
+.overlay {
+    background-color: blue;
+    opacity: 0.6;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+}
+`;
 
 const Jumbotron = () => {
     return (
         <Styles>
-        <Jumbo fluid classname="jumbo">
-            <div classname="overlay"></div>
+        <Jumbo fluid className="jumbo">
+            <div className="overlay"></div>
             <Container>
                 <h1>Welcome to my portfolio</h1>
                 <p>See my previous work</p>
