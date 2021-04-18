c891d034d78b64eec63c431b19359d08ff52f619
Bela Hamid <belahamid@gmail.com>
2021-04-18 08:15:29 +0100

    added more project cards to container

--- src/Containers/Projects.js
+++ src/Containers/Projects.js
@@ -1,25 +1,61 @@
 import React, { Fragment } from 'react';
-import Styled from 'styled-components';
+import styled from 'styled-components';
 import { Card } from 'react-bootstrap';
 
 
 
 const Projects = () => {
+
+    const Styles = styled.div`
+    .color: pink;
+    
+    `;
+
+
+
     return(
         <>
         <h1 style={{color: "#52074a"}}>My Projects</h1>
         <p> Here is a list of projects page</p>
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>Gym Membership</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Web App</Card.Subtitle>
-                <Card.Text>
-                Python | Flask | PostgreSQL
-                </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-        </Card>
+        <Styles>
+            <Card style={{ width: '18rem', margin: '20px' }}>
+                <Card.Body>
+                    <Card.Title>Gym Membership</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Web App</Card.Subtitle>
+                    <Card.Text>
+                    Python | Flask | PostgreSQL
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+        </Styles>
+        <Styles>
+            <Card style={{ width: '18rem', margin: '20px' }}>
+                <Card.Body>
+                    <Card.Title>Space Education</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Web App</Card.Subtitle>
+                    <Card.Text>
+                    JavaScript | Vue | 3rd Party APIs
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+        </Styles>
+        <Styles>
+            <Card style={{ width: '18rem', margin: '20px' }}>
+                <Card.Body>
+                    <Card.Title>Superhero Sightings</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Web App</Card.Subtitle>
+                    <Card.Text>
+                    Java | Spring | h2SQL
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+        </Styles>
 
         </>
     )
