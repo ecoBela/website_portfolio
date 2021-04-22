51ddb4ca13e5b27b2ce2b2ae0ceed34d96327428
Bela Hamid <belahamid@gmail.com>
2021-04-22 01:35:23 +0100

    changed styles on About component

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,32 +3,44 @@ import styled from 'styled-components';
 import photo from '../assets/photo.jpeg';
 
 const Styled = styled.div`
-// background: #52074a;
-// background: linear-gradient(white, grey);
-background: url(${photo});
+background: black;
+// background: #E0E0E0;
+// background: linear-gradient(grey, #52074a);
+// background: url(${photo}) no-repeat;
 height: 100vh;
-color: white;
+display: flex;
+justify-items: center;
+.intro{
+    margin: 1rem;
+}
+
+
+color: #52074a;
 `;
 
 const StyledParagraph = styled.p`
-background: grey;
-opacity: 0.3;
-margin-top:20rem;
+// background: grey;
+// opacity: 0.5;
+margin-top:10rem;
+margin-left: 5rem;
+margin-right: 5rem;
+margin-bottom: 0;
 padding: 1rem;
+text-align: center;
 `;
 
 const About = () => {
     return (
         <>
         <Styled>
-        <h2>Hi!  I'm Bela. </h2>
-        <StyledParagraph>
-        <p>Add photo</p>
-        <p>Background</p>
-        <p>Values</p>
-        <p>Skills</p>
-        <p>Interests</p>
-        </StyledParagraph>
+         <h2 className="intro">Hi! I'm Bela</h2>
+         <img src={photo}/>
+            <StyledParagraph>
+                <p>Background</p>
+                <p>Values</p>
+                <p>Skills</p>
+                <p>Interests</p>
+            </StyledParagraph>
         </Styled>
         </>
     )
